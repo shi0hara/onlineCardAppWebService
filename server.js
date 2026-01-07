4c277e8612457d80ca7de818dc4c551c4a06482d
shi0hara <jeromeho1212@gmail.com>
2026-01-07 15:59:56 +0800

    1

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Server started on port', port);
+});
+
+// Example Route: Get all cards
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allcards' });
+    }
+});
